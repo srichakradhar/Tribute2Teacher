43445e598e6828c854a38960bb95a5bcae8be066
Srichakradhar Reddy Nagireddy <srichakra3nsr3@gmail.com>
2017-02-16 18:00:00 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$('document').ready(function () {
+
+    $('.jumbotron-fluid').height($( window ).height());
+
+    // timeline
+    var options = {
+        numYears:4,
+        startYear:2011,
+        click:function(e,event){
+            alert(event.name);
+        }
+    };
+
+    var timelineDiv = $('#timeline');
+    timelineDiv.jqtimeline(options);
+    var timelineObj = timelineDiv.data('jqtimeline');
+
+    var eventObj = {id:1, name:"Fell in Love with AOL",on : new Date('2012-02-14')};
+    timelineObj.addEvent(eventObj);
+    eventObj = {id:1, name:"Became a teacher",on : new Date('2012-04-01')};
+    timelineObj.addEvent(eventObj);
+    eventObj = {id:1, name:"Left job",on : new Date('2012-06-30')};
+    timelineObj.addEvent(eventObj);
+    eventObj = {id:1, name:"Became a full time teacher",on : new Date('2012-07-07')};
+    timelineObj.addEvent(eventObj);
+    eventObj = {id:1, name:"Got married",on : new Date('2013-03-01')};
+    timelineObj.addEvent(eventObj);
+    eventObj = {id:1, name:"Moved to Hyderabad",on : new Date('2013-04-01')};
+    timelineObj.addEvent(eventObj);
+
+    // one page scroll
+    // $('.main').onepage_scroll({
+    //     sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+    //     easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+    //                                      // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+    //     animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+    //     pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+    //     updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+    //     beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+    //     afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+    //     loop: true,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+    //     keyboard: true,                  // You can activate the keyboard controls
+    //     responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+    //     // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+    //     // the browser's width is less than 600, the fallback will kick in.
+    //     direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
+    // });
+
+});
